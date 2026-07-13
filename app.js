7681b674084baf15b6c4cf6f2337257663648b7d
laxman-khandagale <lkhandagle4@gmail.com>
2026-07-13 15:55:02 +0530

    episode=01

---
+++ app.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("div", { id: "title" },
+    React.createElement("h1", { id: "heading" }, "Namaste React!"),
+    React.createElement("h2", { id: "subheading" }, "This is a subheading"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
