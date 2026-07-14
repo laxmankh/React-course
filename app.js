0e289721b2ea18c5ab29ff8ef46d4615f689fc28
laxman-khandagale <lkhandagle4@gmail.com>
2026-07-14 08:33:16 +0530

    chore: initialize project with package.json and add dependencies

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("div", { id: "title" },
     React.createElement("h1", { id: "heading" }, "Namaste React!"),
     React.createElement("h2", { id: "subheading" }, "This is a subheading"));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
+
