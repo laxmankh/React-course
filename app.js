6fd1c9fbe87b4405e5e55dfb8514db082dc7f79a
laxman-khandagale <lkhandagle4@gmail.com>
2026-07-14 11:32:25 +0530

    refactor: update app.js to use functional components and improve JSX structure; modify hello.html for consistent formatting

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("div", { id: "title" },
-    React.createElement("h1", { id: "heading" }, "Namaste React!"),
-    React.createElement("h2", { id: "subheading" }, "This is a subheading"));
+//in background, react is converting this JSX into React.createElement() function call using babel compiler
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+const Jsxheading = () => {
+  return (
+    <>
+      <h1> Namaste React! using JSX</h1>
+    </>
+  );
+};
 
+const FunctionalComponent = () => {
+  return (
+    <>
+      <Jsxheading />
+      <h1> Namaste React! using functional component</h1>
+    </>
+  );
+};
+root.render(<FunctionalComponent />);
